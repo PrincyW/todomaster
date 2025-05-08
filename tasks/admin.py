d52a7bf2ee4fe7aed525ba5f80db204ae917f293
Princy Wamal <princywamal@gmail.com>
2025-05-08 09:22:47 +0200

    Admin config

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'due_date', 'priority', 'completed')
+    list_filter = ('completed', 'priority', 'category')
+    search_fields = ('title', 'description')
+
+admin.site.register(Category)
+admin.site.register(Task, TaskAdmin)
